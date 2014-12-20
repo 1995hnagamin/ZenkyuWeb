933133368924f13c9aa65efed6e2730357088f04
hnagamin <1995.hnagamin@gmail.com>
2014-12-20 11:46:24 +0900

    Viewを実装した

---
+++ sample.js
@@ -0,0 +1,116 @@
+Data = [
+{
+  courseid: "course1",
+  name: "ドイツ語",
+  dow: "Mon",
+  period: 2
+},
+{
+  courseid: "course2",
+  name: "情報",
+  dow: "Mon",
+  period: 5
+},
+
+{
+  courseid: "course3",
+  name: "人文地理学",
+  dow: "Tue",
+  period: 3
+},
+{
+  courseid: "course4",
+  name: "言語学",
+  dow: "Tue",
+  period: 4
+},
+{
+  courseid: "course5",
+  name: "電子工学",
+  dow: "Tue",
+  period: 5
+},
+{
+  courseid: "course6",
+  name: "有機化学",
+  dow: "Wed",
+  period: 1
+},
+{
+  courseid: "course7",
+  name: "線形代数",
+  dow: "Wed",
+  period: 2
+},
+{
+  courseid: "course8",
+  name: "線形代数",
+  name: "力学",
+  dow: "Wed",
+  period: 4
+},
+{
+  courseid: "course9",
+  name: "線形代数",
+  name: "微分方程式",
+  dow: "Wed",
+  period: 5
+},
+{
+  courseid: "course10",
+  name: "線形代数",
+  name: "ドイツ語",
+  dow: "Thu",
+  period: 2
+},
+{
+  courseid: "course11",
+  name: "線形代数",
+  name: "英語",
+  dow: "Thu",
+  period: 4
+},
+
+{
+  courseid: "course12",
+  name: "線形代数",
+  name: "微分積分学",
+  dow: "Fri",
+  period: 1
+},
+{
+  courseid: "course13",
+  name: "線形代数",
+  name: "微分積分学",
+  dow: "Fri",
+  period: 2
+},
+{
+  courseid: "course14",
+  name: "線形代数",
+  name: "英語",
+  dow: "Fri",
+  period: 3
+}
+];
+
+Presence = [
+{
+  date: new Date("Dec 1, 2014"),
+  courseid: "course1",
+  presence: "Present",
+  memo: "ねてた"
+},
+{
+  date: new Date("Dec 8, 2014"),
+  courseid: "course1",
+  presence: "Absent",
+  memo: "バイトが重なった"
+},
+{
+  date: new Date("Dec 15, 2014"),
+  courseid: "course1",
+  presence: "Canceled",
+  memo: ""
+}
+];
